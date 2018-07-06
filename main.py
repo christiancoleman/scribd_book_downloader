130bb47a22933900cc2a7994339d97ebf8082922
Christian Coleman <6051308+christiancoleman@users.noreply.github.com>
2018-07-06 01:47:11 -0400

    Made it a little better looking by adding bootstrap

--- main.py
+++ main.py
@@ -9,16 +9,34 @@ SCRIBD_CONTENT_URL = "/contents.json?token={token}"
 SCRIBD_IMAGE_URL = "/{filename}?token={token}"
 TAB_CHAR = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
 PATH_FOR_BOOKS = "books"
+HTML_TOP_ALL_PAGES = """
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-# filled in for debug only
-resource_id = "358031167"
-token = "1530676333_3I2UU77FBDF72EYXQHA76QSFICMAJC5YMBHFW676H4ZK3ELF4VQUR2QUSN3Q6===_2ed0e79e41be8e83151f265e6bca3af50d01645f"
-local_name_of_book_directory = "hack_like_god"
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+"""
+HTML_BOTTOM_ALL_PAGES = """
+        </div>
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    </body>
+</html>
+"""
 
 
 def main():
     prompt()
-    #create_book('n', 'n')  # for debug only
 
 
 #######################################################################################################################
@@ -64,8 +82,8 @@ def create_book(use_local_json, images_downloaded):
     global resource_id
     global token
 
-    html_all_content = '<html>'
-    html_toc_content = '<html><h1>Table of Contents</h1>'
+    html_all_content = HTML_TOP_ALL_PAGES
+    html_toc_content = HTML_TOP_ALL_PAGES + '<h1>Table of Contents</h1>'
 
     if use_local_json == "y":
         toc_json_string = read_json_from_disk("{PATH_FOR_BOOKS}/{dir}/toc.json".format(PATH_FOR_BOOKS=PATH_FOR_BOOKS, dir=local_name_of_book_directory))
@@ -79,12 +97,16 @@ def create_book(use_local_json, images_downloaded):
             chapter_raw = read_json_from_disk("{PATH_FOR_BOOKS}/{dir}/{toc_item}.json".format(PATH_FOR_BOOKS=PATH_FOR_BOOKS, dir=local_name_of_book_directory, toc_item=toc_item))
         else:
             chapter_raw = download_chapter_from_scribd(toc_item)
+            save_file(toc_item, chapter_raw, "json")
 
         html_toc_content += create_toc_from_item("{toc_item}.html".format(toc_item=toc_item))
-        html_all_content += create_chapter_from_json(chapter_raw, toc_item, images_downloaded)
+        html_chapter_content = create_chapter_from_json(chapter_raw, toc_item, images_downloaded)
+        html_all_content += html_chapter_content
+        html_chapter_content = HTML_TOP_ALL_PAGES + html_chapter_content + HTML_BOTTOM_ALL_PAGES
+        save_file(toc_item, html_chapter_content, "html")
 
-    html_toc_content += "</html>"
-    html_all_content += "</html>"
+    html_toc_content += HTML_BOTTOM_ALL_PAGES
+    html_all_content += HTML_BOTTOM_ALL_PAGES
 
     save_file("toc", html_toc_content, "html")
     save_file("all", html_all_content, "html")
@@ -226,8 +248,6 @@ def create_chapter_from_json(json_string, chapter_path, images_downloaded):
             print '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
 
             html_chapter_content += "<h1>UNEXPECTED TYPE (" + block_type + ")</h1>"
-
-    save_file(chapter_path, html_chapter_content, "html")
 
     return html_chapter_content
 
