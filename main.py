6aeff846575467f48368b90ede0879177b96b99a
Christian Coleman <6051308+christiancoleman@users.noreply.github.com>
2018-07-07 02:36:57 -0400

    Little dev on automating grab of token - not working yet. Added way to continue downloading if the book is large (re-enter a new token occurs when books take over 5 min to download or 150 MB on my connection).

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+import random
+import re
 import json
 import os
 import sys
@@ -37,6 +44,29 @@ HTML_BOTTOM_ALL_PAGES = """
     </body>
 </html>
 """
+LIST_OF_BOOK_URLS = [
+    "https://www.scribd.com/book/282496355/",
+    "https://www.scribd.com/book/253052099/",
+    "https://www.scribd.com/book/239467568/",
+    "https://www.scribd.com/book/358031167/",
+    "https://www.scribd.com/book/358031166/",
+    "https://www.scribd.com/book/282538161/",
+    "https://www.scribd.com/book/239465986/",
+    "https://www.scribd.com/book/239467888/",
+    "https://www.scribd.com/book/282496455/",
+    "https://www.scribd.com/book/287523516/",
+    "https://www.scribd.com/book/298574302/",
+    "https://www.scribd.com/book/287529182/",
+    "https://www.scribd.com/book/282495159/",
+    "https://www.scribd.com/book/239465548/",
+    "https://www.scribd.com/book/255443250/",
+    "https://www.scribd.com/book/253049325/",
+    "https://www.scribd.com/book/287523437/",
+    "https://www.scribd.com/book/358572397/",
+    "https://www.scribd.com/book/287528768/",
+    "https://www.scribd.com/book/282494944/",
+    "https://www.scribd.com/book/353532808/"
+]
 
 
 def main():
@@ -45,6 +75,8 @@ def main():
     global token
     global use_local_json
     global images_downloaded
+    global driver_url
+    global driver_session_id
     if len(sys.argv) == 2:
         local_name_of_book_directory = sys.argv[1]
         use_local_json = 'y'
@@ -60,6 +92,8 @@ def main():
         print "Creating book from already downloaded json data BUT downloading image data..."
         create_book()
     else:
+        driver_url = ""
+        driver_session_id = ""
         prompt()
 
 
@@ -91,6 +125,9 @@ def prompt():
     global token
     global use_local_json
     global images_downloaded
+    global using_selenium
+
+    using_selenium = False  # setting this pre-emptively as a default value
 
     use_local_json = raw_input("Use local json files? (y or n): ")
     if use_local_json != "y" and use_local_json != "n":
@@ -107,7 +144,11 @@ def prompt():
 
     if use_local_json == "n" or images_downloaded == "n":
         resource_id = raw_input("Enter resource id: ")
-        token = raw_input("Enter your token: ")
+        token = raw_input("Enter your token (leave blank if you'd like to automate it with Selenium (not working ATM)): ")
+        if token == "":
+            print "Getting token..."
+            using_selenium = True
+            get_token_with_selenium()
 
     print "Creating book..."
     create_book()
@@ -156,6 +197,7 @@ def create_book():
     save_file("toc", html_toc_content, "html")
     save_file("all", html_all_content, "html")
 
+
 #######################################################################################################################
 
 
@@ -175,23 +217,17 @@ def download_chapter_from_scribd(chapter_path):
     download_url = SCRIBD_BASE_URL.format(resource_id=resource_id)
     download_url += SCRIBD_CHAPTERS_URL.format(chapter_path=chapter_path)
     download_url += SCRIBD_CONTENT_URL.format(token=token)
-    return urllib2.urlopen(download_url).read()
+    return download_and_handle_expired_token(download_url, False)
 
 
 def download_image_from_scribd(chapter_path, filename):
     print "Downloading " + chapter_path + "/" + filename + "..."
     global resource_id
     global token
-    img = ''
     download_url = SCRIBD_BASE_URL.format(resource_id=resource_id)
     download_url += SCRIBD_CHAPTERS_URL.format(chapter_path=chapter_path)
     download_url += SCRIBD_IMAGE_URL.format(filename=filename, token=token)
-    try:
-        img = urllib2.urlopen(download_url)
-    except urllib2.HTTPError:
-        print_error_message('Photo returned an HTTPError: ' + download_url)
-        pass
-    return img
+    return download_and_handle_expired_token(download_url, True)
 
 
 def download_styles_from_scribd():
@@ -200,7 +236,7 @@ def download_styles_from_scribd():
     global token
     download_url = SCRIBD_BASE_URL.format(resource_id=resource_id)
     download_url += SCRIBD_STYLES_URL.format(token=token)
-    return urllib2.urlopen(download_url).read()
+    return download_and_handle_expired_token(download_url, False)
 
 
 def download_metadata_from_scribd():
@@ -209,7 +245,7 @@ def download_metadata_from_scribd():
     global token
     download_url = SCRIBD_BASE_URL.format(resource_id=resource_id)
     download_url += SCRIBD_METADATA_URL.format(token=token)
-    return urllib2.urlopen(download_url).read()
+    return download_and_handle_expired_token(download_url, False)
 
 
 def download_page_refs_from_scribd():
@@ -218,7 +254,36 @@ def download_page_refs_from_scribd():
     global token
     download_url = SCRIBD_BASE_URL.format(resource_id=resource_id)
     download_url += SCRIBD_PAGE_REFS_URL.format(token=token)
-    return urllib2.urlopen(download_url).read()
+    return download_and_handle_expired_token(download_url, False)
+
+
+def download_and_handle_expired_token(download_url, is_image):
+    global using_selenium
+    global token
+
+    try:
+        if is_image:
+            content = urllib2.urlopen(download_url)
+        else:
+            content = urllib2.urlopen(download_url).read()
+        return content
+    except urllib2.HTTPError:
+        print_error_message('Item returned an HTTPError: ' + download_url)
+        if using_selenium:
+            get_token_with_selenium()
+        else:
+            token = raw_input("Token has expired. Re-enter a valid token: ")
+
+        try:
+            if is_image:
+                content = urllib2.urlopen(download_url)
+            else:
+                content = urllib2.urlopen(download_url).read()
+            return content
+        except urllib2.HTTPError:
+            print_error_message('Failed trying to recover from an expired token')
+        pass
+
 
 #######################################################################################################################
 
@@ -319,12 +384,15 @@ def create_chapter_from_json(json_string, chapter_path):
 
 #######################################################################################################################
 
+
 def return_image_from_node(block, chapter_path):
     image_filename = block['src']
     if images_downloaded == "n":
         img_raw = download_image_from_scribd(chapter_path, image_filename)
-        if img_raw != "":
+        if img_raw is not None:
             save_image(image_filename, img_raw)
+        else:
+            print "Skipped: " + image_filename
     return image_filename
 
 
@@ -359,6 +427,7 @@ def return_text_from_node(block):
         if not found_composite and not found_word:
             print_error_message('Unexpected type found in block_type = \'text\'' + json.dumps(word))
     return paragraph
+
 
 #######################################################################################################################
 
@@ -424,6 +493,69 @@ def print_error_message(msg):
     print msg
     print '00000000000000000000000000000000000000000000000000000000'
     print
+
+
+#######################################################################################################################
+
+
+# not ready yet...
+def get_token_with_selenium():
+    global token
+    global driver_url
+    global driver_session_id
+
+    browser = webdriver.Chrome()
+    if driver_url == "" or driver_session_id == "":
+        driver_url = browser.command_executor._url
+        driver_session_id = browser.session_id
+    else:
+        browser.webdriver.Remote(command_executor=driver_url, desired_capabilities={})
+        browser.session_id = driver_session_id
+
+    browser.get("https://www.scribd.com/login")
+
+    if browser.find_element_by_id("login_or_email") is not None:
+        username_element = browser.find_element_by_id("login_or_email")
+        username_element.clear()
+        username_element.send_keys("ENTER USERNAME")
+        password_element = browser.find_element_by_id("login_password")
+        password_element.clear()
+        password_element.send_keys("ENTER PASSWORD")
+        password_element.send_keys(Keys.RETURN)
+        WebDriverWait(browser, 10).until(
+            expected_conditions.presence_of_element_located((By.CLASS_NAME, "sign_out_btn")))
+
+    browser.get(LIST_OF_BOOK_URLS[random.randint(1, len(LIST_OF_BOOK_URLS) + 1)])
+
+    if browser.find_element_by_class_name("start_reading_btn") is not None:
+        reading_button = browser.find_element_by_class_name("start_reading_btn")
+        reading_button.click()
+        WebDriverWait(browser, 10).until(
+            expected_conditions.presence_of_element_located((By.CLASS_NAME, "icon-ic_displaysettings")))
+
+    source = browser.page_source.encode("utf-8")
+    # token_raw = re.search("\"access_token\":\".*?\"", source)
+    # ^ doesn't return a token that can be used to grab content
+    token_raw = re.search("\?token=\".*?\"", source)
+
+    page_number_counter_old = browser.find_element_by_class_name("page_counter")
+
+    if token_raw is None:
+        left_click_button = browser.find_element_by_class_name("icon-ic_back_arrow")
+        left_click_button.click()
+        WebDriverWait(browser, 10).until(
+            expected_conditions.presence_of_element_located((By.TAG_NAME, "page_counter")))
+
+    print token_raw
+    print token_raw.group()
+    split_results = token_raw.group().split("\"")
+    print split_results
+    # ^ returns ['', 'access_token', ':', '1530935337_3I2UU77KBTEP2EYTQHC6KWCWIWLFVWPBH5GBQ4FYEMZK3ELE5BVUZYATSF2A2===_b7984fc7035c420005762681965bbd394b9ea9c8', '']
+    token = split_results[3]
+    print token
+
+
+#######################################################################################################################
 
 
 main()
